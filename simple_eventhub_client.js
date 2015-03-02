3a153b45934379018291b61b5ea320d996bb0a7d
noodlefrenzy <noodlefrenzy@hotmail.com>
2015-03-02 13:14:51 -0800

    Modify simple client to allow sending or receiving.

--- simple_eventhub_client.js
+++ simple_eventhub_client.js
@@ -14,52 +14,79 @@ if (process.argv.length < 3) {
 } else {
     var settingsFile = process.argv[2];
     var settings = require('./' + settingsFile);
-    assertOptions(settings, [ 'serviceBusHost', 'SASKeyName', 'SASKey', 'eventHubName', 'tableStoreName', 'tableStoreKey']);
+    assertOptions(settings, ['serviceBusHost', 'SASKeyName', 'SASKey', 'eventHubName', 'tableStoreName', 'tableStoreKey']);
 
     var group = settings.consumerGroup || '$Default';
-    var msgVal = Math.floor(Math.random() * 10000);
+    var msgVal = Math.floor(Math.random() * 10000) + 1000000;
 
-    var hub = Sbus.EventHubClient(settings.serviceBusHost, settings.eventHubName, settings.SASKeyName, settings.SASKey);
-    hub.getEventProcessor(group, function(err, processor) {
-        if(err) {
-            console.log("Unable to allocate event processor.  Error: ");
-            console.log(err);
-            process.exit(1);
+    var sender = true;
+    var receiver = true;
+    if (process.argv.length > 3) {
+        if (process.argv[3] === 'send') receiver = false;
+        else if (process.argv[3] === 'receive') sender = false;
+        else throw new Error('Unknown action.');
+    }
+
+    var payloadToSend = {"DataString": "From node-sbus-amqp10", "DataValue": msgVal};
+    var pkToSend = "PK" + msgVal;
+
+    var sendCB = function (tx_err) {
+        if (tx_err) {
+            console.log('Error Sending: ');
+            console.log(tx_err);
         }
+        if (!receiver) {
+            console.log('Sent message with value ' + msgVal + '.  Not receiving, so exiting');
+            process.exit(0);
+        }
+    };
 
-        processor.set_storage(settings.tableStoreName, settings.tableStoreKey);
-        processor.init(function (rx_err, partitionId, payload) {
-            if (rx_err) {
-                console.log('Error receiving: ');
-                console.log(rx_err);
-            } else {
-                console.log('Received (' + partitionId + '): ');
-                console.log(payload);
+    var receiveCB = function (processor, rx_err, partitionId, payload) {
+        if (rx_err) {
+            console.log('Error receiving: ');
+            console.log(rx_err);
+        } else {
+            console.log('Received (' + partitionId + '): ', payload);
+            if (sender) {
+                // Not sending, so no payload to look for.
                 if (payload.DataValue === msgVal) {
-                    console.log('Exiting after receiving expected value ' + msgVal);
-                    processor.teardown(function() {
-                        process.exit(1);
+                    processor.teardown(function () {
+                        console.log('Exiting after receiving expected value ' + msgVal);
+                        process.exit(0);
                     });
                 }
             }
-        }, function(init_err) {
-            if(err) {
-                console.log("Unable to init.  Error: ");
-                console.log(init_err);
-                process.exit(1);
-            } else {
+        }
+    };
+
+    var initCB = function (processor, init_err) {
+        if (init_err) {
+            console.log("Unable to init.  Error: ");
+            console.log(init_err);
+            process.exit(1);
+        } else {
+            if (receiver) {
                 processor.receive();
-                var payloadToSend = { "DataString": "From node-sbus-amqp10", "DataValue": msgVal };
-                //for (var idx=0; idx < 900; ++idx) {
-                //    payloadToSend["K" + idx] = idx;
-                //}
-                processor.send(payloadToSend, "PK2", function(tx_err) {
-                    if (tx_err) {
-                        console.log('Error Sending: ');
-                        console.log(tx_err);
-                    }
-                })
             }
+            if (sender) {
+                processor.send(payloadToSend, pkToSend, sendCB);
+            }
+        }
+    };
+
+    var hub = Sbus.EventHubClient(settings.serviceBusHost, settings.eventHubName, settings.SASKeyName, settings.SASKey);
+    if (sender && !receiver) {
+        hub.send(payloadToSend, pkToSend, sendCB);
+    } else {
+        hub.getEventProcessor(group, function (err, processor) {
+            if (err) {
+                console.log("Unable to allocate event processor.  Error: ");
+                console.log(err);
+                process.exit(1);
+            }
+
+            processor.set_storage(settings.tableStoreName, settings.tableStoreKey);
+            processor.init(receiveCB.bind(null, processor), initCB.bind(null, processor));
         });
-    });
+    }
 }
